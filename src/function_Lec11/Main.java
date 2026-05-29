6bb5fc7ed2a686d77c760d9b3dae12e5f56dbfb4
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-05-30 00:29:21 +0530

    call function

---
+++ src/function_Lec11/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+    public static void main(String [] args){
+         greet();
+         sayHello("Nishant");
+         int num = getNumber();
+         System.out.println(num);
+    }
+
+    // NO input no output 
+
+    static void greet(){
+        System.out.println("Hello");
+        return;
+   }
+  
+   //Input , No Output
+
+  static void sayHello(String name){
+    System.out.println("Hello " + name);
+   }
+ // No Input , Output
+
+ static int getNumber(){
+    return 10;
+ }
+
+ // Input , Output
+ static int multiply(int a,int b){
+    return (a*b);
+ }
+
+
+}
