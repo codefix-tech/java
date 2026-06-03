25afa1f7b04a58a1e475ec8ab77e9235632bac0d
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-03 11:18:21 +0530

    array

--- src/arrayBasic/Print.java
+++ src/arrayBasic/Print.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
 public class Print{
     public static void main(String [] arg){
 
         int [] arr = {2,3,45,6,3,7,3,8,4};
         System.out.println("First element of array :  " + arr[0]);
         System.out.println("Last element of array :  "  + arr[arr.length-1]);
-       
+        System.out.println("Length of array : " + arr.length);
+        
+                System.out.println("Element of array : " + Arrays.toString(arr) );
       
 
        System.out.print("Element of array : ");
