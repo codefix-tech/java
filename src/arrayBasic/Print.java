01366cdc0750113373fc621808578d4cb6b612b8
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-03 11:10:53 +0530

    array

---
+++ src/arrayBasic/Print.java
@@ -0,0 +1,21 @@
+public class Print{
+    public static void main(String [] arg){
+
+        int [] arr = {2,3,45,6,3,7,3,8,4};
+        System.out.println("First element of array :  " + arr[0]);
+        System.out.println("Last element of array :  "  + arr[arr.length-1]);
+       
+      
+
+       System.out.print("Element of array : ");
+      
+        for(int i = 0; i<arr.length;i++){
+                       
+            System.out.print(arr[i] + " ");
+
+            
+
+        }
+        
+    }
+}
