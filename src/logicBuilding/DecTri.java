1defcf927a56c4c02b5c9dd6f663bb1f9e7cdec1
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-04 15:06:14 +0530

    pattern

---
+++ src/logicBuilding/DecTri.java
@@ -0,0 +1,32 @@
+public class DecTri{
+    public static void main(String [] args){
+         
+
+         // increasing triangle of star
+
+         int n = 10;
+
+         for(int i = 0 ; i < n ; i++){
+                 
+                 for(int j = 0 ; j < n-i-1 ; j++){
+
+
+                    System.out.print(" ");
+                    
+                      
+                 }
+                 for(int k = 0; k<= i; k++){
+                      System.out.print("*");
+
+                 }
+                 System.out.println();
+                  
+     
+
+         }
+
+
+        
+
+    }
+}
---
+++ src/logicBuilding/IncreasingTri.java
@@ -0,0 +1,28 @@
+public class IncreasingTri{
+    public static void main(String [] args){
+         
+
+         // increasing triangle of star
+
+         int n = 10;
+
+         for(int i = 0 ; i < n ; i++){
+                 
+                 for(int j = 0 ; j < i ; j++){
+
+
+                    System.out.print("*");
+                    
+                      
+                 }
+                 System.out.println();
+                  
+     
+
+         }
+
+
+        
+
+    }
+}
