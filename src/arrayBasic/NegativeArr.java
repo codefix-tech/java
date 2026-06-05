8f48fbc51d62590d34b44c473257b7cc70dadd1d
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-05 11:27:12 +0530

    basic

---
+++ src/arrayBasic/NegativeArr.java
@@ -0,0 +1,19 @@
+public class NegativeArr{
+    public static void main(String [] arg){
+        
+        int neg = 0;
+
+        int [] arr = {2,-1,-2,9,99,-9,-8};
+        System.out.print("Negative number : ");
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] < 0){
+                System.out.print(arr[i]);
+            }
+            
+        }
+        
+
+
+
+    }
+}
--- src/arrayBasic/OutputInputArray.java
+++ src/arrayBasic/OutputInputArray.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class OutputInputArray{
     public static void main(String [] arg){
         
-        int [] arr = {1,2,43,45,55,66};
+        // print all element
 
-        for(int i = 0 ; i <= 5 ; i++){
+        // int [] arr = {1,2,43,45,55,66};
+
+        // for(int i = 0 ; i <= 5 ; i++){
+        //     System.out.print(arr[i] + " ");
+        // }
+        System.out.print(" Size of Array : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        System.out.print("Enter Array");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt(); 
+           
+        }
+        for(int i = 0 ; i < n ; i++){
             System.out.print(arr[i] + " ");
         }
+
+
+
 
 
          
