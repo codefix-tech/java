ae3a0ed91b5c17730bd58ff4729b89b60eaca0fa
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-08 20:27:50 +0530

    pattern

---
+++ src/pattern/P1.java
@@ -0,0 +1,14 @@
+public class P1{
+    public static void main(String [] arg){
+       
+       int n = 4;
+       int m = 8;
+       for(int i = 0 ; i < n ; i++){
+        for(int j = 1; j < m ; j++){
+            System.out.print("*");
+        }
+        System.out.println(" ");
+       }
+
+    }
+}
---
+++ src/pattern/Pattern.java
@@ -0,0 +1,24 @@
+public class Pattern{
+    public static void main(String [] arg){
+
+    // print single star(*)
+
+    System.out.println("*");
+
+
+   // print four star 
+
+   System.out.println("****");
+
+
+   // print n star
+    int n = 10;
+   for(int i = 0 ; i < n;i++){
+    System.out.print("*");
+   }
+
+
+   
+
+    }
+}
