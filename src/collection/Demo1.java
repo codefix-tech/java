2dd77d61a12569dc11785f1fc85d77e899d43e1d
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-21 21:48:55 +0530

    collection

---
+++ src/collection/Demo1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Demo1{
+    public static void main(String [] arg){
+        Collection<Integer> c = new ArrayList<>();
+        c.add(1);
+        c.add(2);
+        c.add(3);
+
+    }
+}
